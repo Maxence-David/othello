73d9f354b7ca5c96240432c11d20ed25105097e7
Lise Kleiber <lise.kleiber@insa-rouen.fr>
2019-12-08 12:21:41 +0100

    implémentation en c de definirCouleurNouveauJoueur

---
+++ src/definirCouleurNouveauJoueur.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "couleur.h"
+
+CO_Couleur definirCouleurNouveauJoueur(laCouleur Couleur){
+  CO_Couleur blanc, noir;
+
+  if (laCouleur = blanc){
+    return(noir);
+  }
+  else {
+    return(blanc);
+  }
+}
