35df5dcb6b0ad67054d06704ef1228f92ccb1fc9
Gauthier Imbert <gauthier.imbert@insa-rouen.fr>
2019-12-12 15:38:32 +0100

    Implémentation affichagePlateau.h

---
+++ include/affichagePlateau.h
@@ -0,0 +1,14 @@
+#ifndef __AFFICHAGE_PLATEAU__
+#define __AFFICHAGE_PLATEAU__
+
+#include "plateau.h"
+
+/**
+ * \fn  affichagePlateau()
+ * \brief Fonction qui affiche le plateau dans le terminal
+ * \param PL_Plateau    Le plateau à afficher
+ * \return void
+ */
+void OTH_affichagePlateau(PL_Plateau plateau);
+
+#endif
