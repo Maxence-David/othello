04ef8ce732c48ba037b37aec76389b3e655d59d7
Gauthier Imbert <gauthier.imbert@insa-rouen.fr>
2019-12-12 14:46:06 +0100

    Modification du code pour améliorer lisibilité

--- src/placerCoup.c
+++ src/placerCoup.c
@@ -76,25 +76,27 @@ int OTH_testModifDirection(PL_Plateau plateau, C_Coup coup, Direction dir)
         break;
     }
     positionAtester = PO_defPosition(PO_ObtenirX(C_Obtenir_Position_Coup(coup)) + incrementX, PO_ObtenirY(C_Obtenir_Position_Coup(coup)) + incrementY); /*La première postion à tester est la position du pion à laquelle on ajoute la valeur des incréments X et Y*/
-    if (C_Obtenir_Couleur_Coup(coup) == NOIR)   /*Si la couleur du joueur courant est BLANC, alors celle de l'adversaire est nécéssairement NOIR et inversément*/
-    {
-        couleurAdverse = BLANC;
-    }
-    else
-    {
-        couleurAdverse = NOIR;
-    }
-    if ((PI_ObtenirEtatPion(PL_ObtenirPion(positionAtester, plateau))) && (C_Obtenir_Couleur_Coup(coup) == PI_ObtenirCouleurPion(PL_ObtenirPion(positionAtester, plateau))))    /*Si le pion juste à coté du coup que l'on place est actif et de la même couleur, alors on ne va retourner aucun pion dans cette direction */
+    
+    couleurAdverse = obtenirCouleurAdverse(coup);   /*On affecte à couleurAdverse la couleur de l'adversaire*/
+    
+    if ((PI_ObtenirEtatPion(PL_ObtenirPion(positionAtester, plateau))) && 
+        (C_Obtenir_Couleur_Coup(coup) == PI_ObtenirCouleurPion(PL_ObtenirPion(positionAtester, plateau))))    /*Si le pion juste à coté du coup que l'on place est actif et de la même couleur, alors on ne va retourner aucun pion dans cette direction */
     {
         return 0;
     }
     else
     {
-        while((PI_ObtenirEtatPion(PL_ObtenirPion(positionAtester, plateau))) && (PI_ObtenirCouleurPion(PL_ObtenirPion(positionAtester, plateau)) == couleurAdverse) && (0 <= PO_ObtenirX(positionAtester)) && (PO_ObtenirX(positionAtester) <= 8) && (0 <= PO_ObtenirY(positionAtester)) && (PO_ObtenirY(positionAtester) <= 8))    /*On teste les positions suivantes tant que les pions sont actifs, de la couleur adverse et que l'on ne sort pas du plateau*/
+        while((0 <= PO_ObtenirX(positionAtester)) && (PO_ObtenirX(positionAtester) <= 8) && 
+            (0 <= PO_ObtenirY(positionAtester)) && (PO_ObtenirY(positionAtester) <= 8) && 
+            (PI_ObtenirEtatPion(PL_ObtenirPion(positionAtester, plateau))) && 
+            (PI_ObtenirCouleurPion(PL_ObtenirPion(positionAtester, plateau)) == couleurAdverse))    /*On teste les positions suivantes tant que les pions sont actifs, de la couleur adverse et que l'on ne sort pas du plateau*/
         {
             positionAtester = PO_defPosition(PO_ObtenirX(positionAtester) + incrementX, PO_ObtenirY(positionAtester) + incrementY); /*La position suivante à tester est obtenue en ajoutant les incréments X et Y à la postition actuelle*/
         }
-        if ((PI_ObtenirEtatPion(PL_ObtenirPion(positionAtester, plateau)) != 0) && (PI_ObtenirCouleurPion(PL_ObtenirPion(positionAtester, plateau)) == C_Obtenir_Couleur_Coup(coup)))   /*Lorsqu'on atteint une poistion ne respectant pas les conditions mais qui est toujours dans le plateau, on regarde la couleur et l'état du pion associé*/
+        if ((0 <= PO_ObtenirX(positionAtester)) && (PO_ObtenirX(positionAtester) <= 8) &&
+            (0 <= PO_ObtenirY(positionAtester)) && (PO_ObtenirY(positionAtester) <= 8) &&
+            (PI_ObtenirEtatPion(PL_ObtenirPion(positionAtester, plateau)) != 0) && 
+            (PI_ObtenirCouleurPion(PL_ObtenirPion(positionAtester, plateau)) == C_Obtenir_Couleur_Coup(coup)))   /*Lorsqu'on atteint une poistion ne respectant pas les conditions mais qui est toujours dans le plateau, on regarde la couleur et l'état du pion associé*/
         {
             return 1;   /*Si le pion est de la couleur du joueur et est actif, alors il y aura au moins une modification*/
         }
@@ -102,5 +104,17 @@ int OTH_testModifDirection(PL_Plateau plateau, C_Coup coup, Direction dir)
         {
             return 0;
         }
+    }
+}
+
+CO_Couleur obtenirCouleurAdverse(C_Coup coup)
+{
+    if (C_Obtenir_Couleur_Coup(coup) == NOIR)   /*Si la couleur du joueur courant est BLANC, alors celle de l'adversaire est nécéssairement NOIR et inversément*/
+    {
+        return BLANC;
+    }
+    else
+    {
+        return NOIR;
     }
 }
