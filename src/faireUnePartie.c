b5474fc2c68b5b3c1e8de6002e5e3818d7695bdd
Lise Kleiber <lise.kleiber@insa-rouen.fr>
2019-12-16 19:43:44 +0100

    corrections de faireUnePartie.c

--- src/faireUnePartie.c
+++ src/faireUnePartie.c
@@ -6,38 +6,37 @@
 CO_Couleur faireUnePartie(){
 
   CO_Couleur couleurDuGagnant, joueurCourant;
-  int joueurOuIA;
+  int contreIA;
   PL_Plateau unPlateau;
   C_Coup coupIA;
 
-  joueurOuIA = getchar();
-  caractereEnEntier(joueurOuIA);
+  contreIA = caractereEnEntier(getchar()); /* si l'utilisateur veut joueur contre l'IA ou contre un autre joueur*/
   joueurCourant = noir; /* les noirs commencent (cf règles de l'othello)*/
   unPlateau = PL_creerPlateau();
-  if(joueurContreIA == 0){ 
+  if(contreIA == 0){ 
     while(partieTerminee(unPlateau,joueurCourant) == 0){
-      coupJoueur1 = placerCoup(unPlateau, !(partieTerminee(unPlateau, joueurCourant)));
+      coupJoueur1 = placerCoup(unPlateau, partieTerminee(unPlateau, joueurCourant));
       majPlateau(unPlateau, coupJoueur1);
       affichagePlateau(unPlateau);
       joueurCourant = C_Obtenir_Couleur_Coup(coupJoueur1);
-      coupJoueur2 = placerCoup(unPlateau, !(partieTerminee(unPlateau, joueurCourant)));
+      coupJoueur2 = placerCoup(unPlateau, partieTerminee(unPlateau, joueurCourant));
       majPlateau(unPlateau, coupJoueur2);
       affichagePlateau(unPlateau);
       joueurCourant = C_Obtenir_Couleur_Coup(coupJoueur1);
     }
-    else {
-      if(joueurContreIA == 0){ 
-	while(partieTerminee(unPlateau,joueurCourant) == 0){
-	  coupJoueur = placerCoup(unPlateau, !(partieTerminee(unPlateau, joueurCourant)));
-	  majPlateau(unPlateau, coupJoueur);
-	  affichagePlateau(unPlateau);
-	  joueurCourant = C_Obtenir_Couleur_Coup(coupJoueur);
-	  coupIA  = moduleIA(unPlateau);
-	  majPlateau(unPlateau, coupJoueur2);
-	  affichagePlateau(unPlateau);
-	  joueurCourant = C_Obtenir_Couleur_Coup(coupIA);
-	}
+   else {
+    if(contreIA != 0){ 
+	   while(partieTerminee(unPlateau,joueurCourant) == 0){
+      coupJoueur = placerCoup(unPlateau, partieTerminee(unPlateau, joueurCourant));
+      majPlateau(unPlateau, coupJoueur);
+      affichagePlateau(unPlateau);
+      joueurCourant = C_Obtenir_Couleur_Coup(coupJoueur);
+      coupIA  = moduleIA(unPlateau);
+      majPlateau(unPlateau, coupJoueur2);
+      affichagePlateau(unPlateau);
+      joueurCourant = C_Obtenir_Couleur_Coup(coupIA);
       }
     }
-    return(obtenirCouleurGagnant(unPlateau));
   }
+    return(obtenirCouleurGagnant(unPlateau));
+}
