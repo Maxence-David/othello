82ff8955fc0ebc5f2eb8f0589ff1c35aa18666a9
Gauthier Imbert <gauthier.imbert@insa-rouen.fr>
2019-12-22 15:53:11 +0100

    Implementation obtenirCouleurGagnant.h

---
+++ include/obtenirCouleurGagnant.h
@@ -0,0 +1,11 @@
+#include "couleur.h" 
+#include "plateau.h"
+
+/**
+ * \fn  OTH_obtenirCouleurGagnant()
+ * \brief Fonction qui permet de savoir si il y a un gangnant, et si oui quelle est sa couleur 
+ * \param PL_Plateau    Le plateau
+ * \return int,CO_Couleur   Un boolén pour savoir si il y a un gagnant et sa couleur   
+ */
+void OTH_obtenirCouleurGagnant(PL_Plateau plateau, int* ilYaUnGagnant, CO_Couleur* couleurGagnant);
+
