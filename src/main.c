2d56de10646c505f3292136ed3a38d6a4a10fbe2
David Maxence <maxence.david@insa-rouen.fr>
2019-12-27 14:50:19 +0100

    ajout main

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "faireUnePartie.h"
+
+int  main(int argc, char** argv) {
+    CO_Couleur couleurGagnante;
+    CO_Couleur couleurJoueur;
+    if  ( (argc==3) 
+        && ((strcmp(argv[1],"JoueurVSJoueur"))||(strcmp(argv[1],"JoueurVSia"))||(strcmp(argv[1],"iaVSia")))
+        &&((strcmp(argv[2],"noir")) || (strcmp(argv[2],"blanc")) )){
+            if (strcmp(argv[1],"JoueurVSJoueur")){
+                couleurGagnante = OTH_faireUnePartie_J_VS_J(OTH_affichagePlateau,OTH_placerCoup );
+            }
+            if (strcmp(argv[2],"blanc")){
+                couleurJoueur = CO_Blanc();
+            }
+            else {
+                couleurJoueur = CO_Noir();
+            }
+            if (strcmp(argv[1],"JoueurVSia")){
+                couleurGagnante = OTH_faireUnePartie_J_VS_IA(OTH_affichagePlateau,OTH_placerCoup,IA_CoupIA);
+
+            }
+            else
+            {
+                couleurGagnante = OTH_Tournoi(OTH_entrerCoupTournoi,IA_CoupIA,couleurJoueur);
+            }
+            
+        }
+
+
+
+	return EXIT_SUCCESS;
+
+}
