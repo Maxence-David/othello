5c317a35c9aa3d41dab34dede8f722b43a2d4fd0
Gauthier Imbert <gauthier.imbert@insa-rouen.fr>
2019-12-28 18:36:32 +0100

    Dépplacement et modifications OTH_entrerCoup

--- src/affichagePlateau.c
+++ src/affichagePlateau.c
@@ -3,6 +3,8 @@
 #include <string.h>
 
 #include "plateau.h"
+#include "coup.h"
+#include "caractereEnEntier.h"
 #include <stdio.h>
 
 void OTH_affichagePlateau(PL_Plateau plateau)
@@ -42,11 +44,23 @@ void OTH_affichagePlateau(PL_Plateau plateau)
 }
 
 
-void OTH_entrerCoup(PO_Position* position){
-    
+PO_Position OTH_entrerCoup(CO_Couleur couleurJoueurCourant)
+{
+    char coordCoup[10];	/**<On prévoit une chaine de caractère plus grande au cas où l'utilisateur entre n'importe quoi*/
+	int entierColonnes, entierLignes;
+    PO_Position positionDuCoup; 
+
+	do {
+		printf("Joueur %s c'est à vous de jouer, entrez les coordonnées de votre coup svp : ", couleurJoueurCourant);
+		scanf("%s", coordCoup);
+	}	while (!OTH_chaineValide(coordCoup));
+
+	OTH_chaineEnEntiers(coordCoup, entierColonnes, entierLignes);
+	
+	positionDuCoup = PO_defPosition(entierColonnes,entierLignes),PI_CreerPion(couleurJoueurCourant);
+
+    return positionDuCoup;
 }
-
-
 
 void OTH_entrerCoupTournoi(PO_Position* position, int* res){
     
