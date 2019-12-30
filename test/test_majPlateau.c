34c8317d583ebff87470677a5c39cb0fd34afd7d
Gauthier Imbert <gauthier.imbert@insa-rouen.fr>
2019-12-30 12:31:06 +0100

    Implémntation test_majPlateau

--- test/test_majPlateau.c
+++ test/test_majPlateau.c
@@ -41,3 +41,55 @@ void test_changement_couleur_pion_noir_vers_blanc(void)
     CU_ASSERT_EQUAL(resultatAttendu, resultatObtenu);
 }
 
+void test_pour_maj_plateau(void)
+{
+    int resultatAttendu = 1;
+    int resultatObtenu = 0;
+
+    int i, j;
+    PL_Plateau lePlateau;
+    C_Coup leCoup;
+    PI_Pion pionNoir = PI_CreerPion(CO_Noir());
+    PI_Pion pionBlanc = PI_CreerPion(CO_Blanc());
+
+    PL_Initialiser_Plateau(lePlateau);  /**<Création d'une configuration particulière dans laquelle la pose d'un pion va en retourner 8 (un dans chaque direction)*/
+    for(i=1; i<=5; i++)
+    {
+        PL_PoserPion(pionNoir, PO_defPosition(i,1),&lePlateau);
+        PL_PoserPion(pionNoir, PO_defPosition(i,5),&lePlateau);
+    }
+    for(i=2; i<=4; i++)
+    {
+        PL_PoserPion(pionNoir, PO_defPosition(1,i),&lePlateau);
+        PL_PoserPion(pionNoir, PO_defPosition(5,1),&lePlateau);
+    }
+    for(i=2; i<=4; i++)
+    {
+        PL_PoserPion(pionBlanc, PO_defPosition(i,2),&lePlateau);
+        PL_PoserPion(pionBlanc, PO_defPosition(i,4),&lePlateau);
+    }
+    PL_PoserPion(pionBlanc, PO_defPosition(2,3),&lePlateau);
+    PL_PoserPion(pionBlanc, PO_defPosition(4,3),&lePlateau);
+
+    leCoup = C_InitCoup(PO_defPosition(3,3),pionNoir);
+
+    OTH_majPlateau(&lePlateau, leCoup); /**<Le coup a été placé de manière à ce qu'il rende tous les autres pions noirs*/
+
+    for(i=1; i<=5; i++)
+    {
+        for(j=1; j<=5; j++)
+        {
+            if(estBlanc(PI_ObtenirCouleurPion(PL_ObtenirPion(PO_defPosition(i,j),lePlateau))))  /**On vérifie qu'il n'y a aucun pion blanc<*/
+            {
+                resultatObtenu = 0; /**<Si il y a un pion blanc, resultatObtenu reste à 0*/
+            }
+            else
+            {
+                resultatObtenu = 1; /**<Si tous les pions sont noir, resultatObtenu passe à 1*/
+            }
+            
+        }
+    }
+
+    CU_ASSERT_EQUAL(resultatAttendu, resultatObtenu);
+}
