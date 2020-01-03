0cb02573338f1e652c84421dbc194b40e42d8ea4
Lise Kleiber <lise.kleiber@insa-rouen.fr>
2020-01-03 21:20:02 +0100

    correction inversion JoueurVSJoueur et JoueurVSia

--- src/main.c
+++ src/main.c
@@ -2,30 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include "faireUnePartie.h"
+#include "plateau.h"
+#include "couleur.h"
 
 int  main(int argc, char** argv) {
     CO_Couleur couleurGagnante;
     CO_Couleur couleurJoueur;
-    PL_Plateau pl;
+
 /*
     couleurGagnante = OTH_faireUnePartie_J_VS_J(OTH_affichagePlateau,OTH_placerCoup );
     affichageFinPartie(couleurGagnante);*/
 
     if  ( (argc==3) 
-        && ((strcmp(argv[1],"JoueurVSJoueur"))||(strcmp(argv[1],"JoueurVSia"))||(strcmp(argv[1],"iaVSia")))
-        &&((strcmp(argv[2],"noir")) || (strcmp(argv[2],"blanc")) )){
-            if (strcmp(argv[1],"JoueurVSJoueur")){
+        && ((strcmp(argv[1],"JoueurVSJoueur")==0)||(strcmp(argv[1],"JoueurVSia")==0)||(strcmp(argv[1],"iaVSia")==0))
+        &&((strcmp(argv[2],"noir")==0) || (strcmp(argv[2],"blanc")==0) )){
+            if (strcmp(argv[1],"JoueurVSJoueur")==0){
                 printf("ok \n");
                 couleurGagnante = OTH_faireUnePartie_J_VS_J(OTH_affichagePlateau,OTH_placerCoup );
                 affichageFinPartie(couleurGagnante);
             }
-            if (strcmp(argv[2],"blanc")){
+            if (strcmp(argv[2],"blanc")==0){
                 couleurJoueur = CO_Blanc();
             }
             else {
                 couleurJoueur = CO_Noir();
             }
-            if (strcmp(argv[1],"JoueurVSia")){
+            if (strcmp(argv[1],"JoueurVSia")==0){
                 couleurGagnante = OTH_faireUnePartie_J_VS_IA(OTH_affichagePlateau,OTH_placerCoup,IA_CoupIA);
                 affichageFinPartie(couleurGagnante);
             }
