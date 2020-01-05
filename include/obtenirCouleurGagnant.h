8c4363e478343e493040b08e75243711c72c1b37
Gauthier Imbert <gauthier.imbert@insa-rouen.fr>
2020-01-05 18:04:19 +0100

    Modification de l'entête de chacune des fonctions de obtenirCouleurGagnant pour améliorer la documentation

--- include/obtenirCouleurGagnant.h
+++ include/obtenirCouleurGagnant.h
@@ -13,7 +13,7 @@
 #include "plateau.h"
 
 /**
- * \fn  OTH_obtenirCouleurGagnant()
+ * \fn  void OTH_obtenirCouleurGagnant(PL_Plateau plateau, int* ilYaUnGagnant, CO_Couleur* couleurGagnant)
  * \brief Fonction qui permet de savoir si il y a un gangnant, et si oui quelle est sa couleur 
  * \param PL_Plateau    Le plateau
  * \return int,CO_Couleur   Un boolén pour savoir si il y a un gagnant et sa couleur   
