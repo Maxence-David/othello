9fa0c55840bc57e2cddc925d7ada9fd3026a7ab2
Gauthier Imbert <gauthier.imbert@insa-rouen.fr>
2020-01-05 17:44:58 +0100

    Modification de l'entête de chacune des fonctions de caractereEnEntier pour améliorer la documentation

--- include/caractereEnEntier.h
+++ include/caractereEnEntier.h
@@ -10,7 +10,7 @@
 #define __CARACTERE_EN_ENTIER__
 
 /**
- * \fn  OTH_chaineValide()
+ * \fn  int OTH_chaineValide(char* chaine)
  * \brief Fonction verifiant la validité d'une chaine de caractère
  * \param char* chaine  La chaine que l'on teste
  * \return int  Un booléen (1=Vrai si la chaine est valide)
@@ -18,7 +18,7 @@
 int OTH_chaineValide(char* chaine);
 
 /**
- * \fn  OTH_chauneEnEntier()
+ * \fn  void OTH_chaineEnEntiers(char* chaine, int* entier1, int* entier2)
  * \brief Fonction pour convertir une chaine de 2 caractères en deux entiers
  * \param char* chaine  La chaine que l'on veut convertir
  * \param int* entier1  Le premier entier
